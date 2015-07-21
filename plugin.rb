2a6dd8d50518c96e1c168e6d9df13593c4425a8f
Ondřej Surý <ondrej@sury.org>
2015-07-21 16:49:48 +0200

    Simplify the mojeID plugin

--- plugin.rb
+++ plugin.rb
@@ -6,42 +6,13 @@
 
 gem 'omniauth-mojeid'
 
-class mojeIDAuthenticator < ::Auth::Authenticator
+class mojeIDAuthenticator < ::Auth::OAuth2Authenticator
 
   CLIENT_ID = ''
   CLIENT_SECRET = ''
 
   def name
     'mojeid'
-  end
-
-  def after_authenticate(auth_token)
-    result = Auth::Result.new
-
-    # grap the info we need from omni auth
-    data = auth_token[:info]
-    name = data["first_name"]
-    mojeID_uid = auth_token["uid"]
-    email = data['email']
-
-    # plugin specific data storage
-    current_info = ::PluginStore.get("mojeID", "mojeID_uid_#{mojeID_uid}")
-
-    result.user =
-      if current_info
-        User.where(id: current_info[:user_id]).first
-      end
-
-    result.name = name
-    result.extra_data = { mojeID_uid: mojeID_uid }
-    result.email = email
-
-    result
-  end
-
-  def after_create_account(user, auth)
-    data = auth[:extra_data]
-    ::PluginStore.set("mojeID", "mojeID_uid_#{data[:mojeID_uid]}", {user_id: user.id })
   end
 
   def register_middleware(omniauth)
@@ -51,24 +22,41 @@ class mojeIDAuthenticator < ::Auth::Authenticator
   end
 end
 
+class OmniAuth::Strategies::mojeID < OmniAuth::Strategies::OAuth2
+  # Give your strategy a name.
+  option :name, "mojeid"
+
+  # This is where you pass the options you would pass when
+  # initializing your consumer from the OAuth gem.
+  option :client_options, site: 'https://mojeid.fred.nic.cz/'
+
+  # These are called after authentication has succeeded. If
+  # possible, you should try to set the UID without making
+  # additional calls (if the user id is returned with the token
+  # or as a URI parameter). This may not be possible with all
+  # providers.
+  uid { raw_info['id'].to_s }
+
+  info do
+    {
+      :name => raw_info['name'],
+      :email => raw_info['email']
+    }
+  end
+
+  extra do
+    {
+      'raw_info' => raw_info
+    }
+  end
+
+  def raw_info
+    @raw_info ||= access_token.get('/oidc/token/').parsed
+  end
+end
 
 auth_provider :title => 'with mojeID Account',
     :message => 'Log in via mojeID Account (Make sure pop up blockers are not enabled).',
     :frame_width => 920,
     :frame_height => 800,
-    :authenticator => mojeIDAuthenticator.new
-
-
-# We ship with zocial, it may have an icon you like http://zocial.smcllns.com/sample.html
-#  in our current case we have an icon for vk
-register_css <<CSS
-
-.btn-social.mojeID {
-  background: #46698f;
-}
-
-.btn-social.mojeID:before {
-  content: "N";
-}
-
-CSS
+    :authenticator => mojeIDAuthenticator.new('mojeid', trusted: true, auto_create_account: true)
